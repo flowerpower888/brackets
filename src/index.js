0e22117d71c40fe8c05b865e48b0626db3ac9df0
flowerpower888 <oingooboingo@gmail.com>
2021-02-04 21:03:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const opening = [],
+        closing = [];
+  bracketsConfig.map((bracket, i) => {
+    opening.push(bracket[0]);
+    closing.push(bracket[1]);
+  });
+  str = [...str];
+  
+  for (let i = 0; i < str.length; i++) {
+    
+    for (let j = 0; j < opening.length; j++) {
+      if (str[i] == opening[j]) {
+        if (str[i + 1] == closing[j]) {
+          str.splice(i, 2)
+          i -= 2; 
+        }
+      }
+    }
+
+  }
+  return str.length == 0 ? true : false
 }
